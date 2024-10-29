f635041a563abc8423c9d5154bfa970ccaca74c4
Dan Federman <dfed@me.com>
2024-10-29 09:00:21 -0700

    Update simulator names

--- Scripts/build.swift
+++ Scripts/build.swift
@@ -31,7 +31,7 @@ enum Platform: String, CaseIterable, CustomStringConvertible {
 	var destination: String {
 		switch self {
 		case .iOS_18:
-			"platform=iOS Simulator,OS=18.0,name=iPad Pro (12.9-inch) (6th generation)"
+			"platform=iOS Simulator,OS=18.0,name=iPad (10th generation)"
 
 		case .tvOS_18:
 			"platform=tvOS Simulator,OS=18.0,name=Apple TV"
@@ -41,7 +41,7 @@ enum Platform: String, CaseIterable, CustomStringConvertible {
 			"platform=OS X"
 
 		case .watchOS_11:
-			"OS=11.0,name=Apple Watch Series 7 (45mm)"
+			"OS=11.0,name=Apple Watch Series 10 (45mm)"
 
 		case .visionOS_2:
 			"OS=2.0,name=Apple Vision Pro"
